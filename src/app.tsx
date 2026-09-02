87d763363550b179aa97368dfefea6fb08ea4de9
bernardobbm <bernardobbm334@gmail.com>
2026-09-01 23:15:19 -0300

    refactor: Remoção de código de autenticação para novo arquivo
    Remoção do código responsável pelo formulário de inicio de sessão
    para pasta e arquivo específico.

--- src/app.tsx
+++ src/app.tsx
@@ -1,82 +1,17 @@
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
+import { createRouter, RouterProvider } from '@tanstack/react-router';
 
-import { TextInput } from './components/text-input';
+import { routeTree } from './routeTree.gen';
+
 import './styles/global.css';
 
-const authenticateUserFormSchema = z.object({
-  email: z
-    .string()
-    .nonempty('O e-mail é obrigatório')
-    .trim()
-    .refine((email) => z.email().safeParse(email).success, 'E-mail inválido'),
-  password: z
-    .string()
-    .nonempty('A senha precisa ser preenchida')
-    .min(6, 'A senha deve ter pelo menos 6 caracteres'),
-});
+export const router = createRouter({ routeTree });
 
-type AuthenticateUserFormProps = z.infer<typeof authenticateUserFormSchema>;
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 export function App() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: zodResolver(authenticateUserFormSchema),
-  });
-
-  function authenticateUser(data: AuthenticateUserFormProps) {
-    console.log(data);
-  }
-
-  return (
-    <main className='bg-zinc-50font-inter flex h-screen flex-col items-center gap-2 p-4 sm:items-baseline'>
-      <div className='grid w-full grid-flow-row grid-cols-[30%_70%] grid-rows-1 place-items-center gap-5 md:grid-cols-[20%_60%_20%]'>
-        <img
-          className='justify-self-start md:h-20 md:w-38'
-          src='../src/assets/images/horizontal-logo.png'
-          alt='Logo'
-        />
-        <h1 className='font-inter-semibold text-xl text-wrap text-gray-900 sm:text-2xl'>
-          Sistema Gerenciador de Notas - Uniceplac
-        </h1>
-      </div>
-
-      <form
-        className='flex h-full w-fit flex-col items-center justify-center gap-6 px-4'
-        onSubmit={handleSubmit(authenticateUser)}
-      >
-        <TextInput
-          label='Acesso:'
-          id='email'
-          type='email'
-          placeholder='Digite seu e-mail'
-          {...register('email')}
-          error={!!errors.email}
-          errorMessage={errors.email?.message}
-        />
-
-        <TextInput
-          label='Senha:'
-          id='password'
-          type='password'
-          name='password'
-          placeholder='Digite sua senha'
-          {...register('password')}
-          error={!!errors.password}
-          errorMessage={errors.password?.message}
-        />
-
-        <button
-          type='submit'
-          className='bg-primary-green-400 font-inter-bold hover:bg-primary-green-500 h-10 w-full transform rounded-sm text-white shadow-sm transition duration-300 hover:shadow-md'
-        >
-          Entrar
-        </button>
-      </form>
-    </main>
-  );
+  return <RouterProvider router={router} />;
 }
--- src/pages/__auth/sign-in.tsx
+++ src/pages/__auth/sign-in.tsx
@@ -1,9 +1,92 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import { createFileRoute } from '@tanstack/react-router';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import UniceplacLogo from '@/assets/images/uniceplac-logo.svg?react';
+import { TextInput } from '@/components/text-input';
 
 export const Route = createFileRoute('/__auth/sign-in')({
-  component: RouteComponent,
+  component: SignIn,
+  head: () => ({
+    meta: [
+      {
+        title: 'Sign-In - Sistema Gerenciador de Notas',
+      },
+    ],
+  }),
 });
 
-function RouteComponent() {
-  return <div>Hello "/__auth/sign-in"!</div>;
+const SignInFormSchema = z.object({
+  email: z
+    .string()
+    .nonempty('O e-mail é obrigatório')
+    .trim()
+    .refine((email) => z.email().safeParse(email).success, 'E-mail inválido'),
+  password: z
+    .string()
+    .nonempty('A senha precisa ser preenchida')
+    .min(6, 'A senha deve ter pelo menos 6 caracteres'),
+});
+
+type SignInFormProps = z.infer<typeof SignInFormSchema>;
+
+function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(SignInFormSchema),
+  });
+
+  function authenticateUser(data: SignInFormProps) {
+    console.log(data);
+  }
+
+  return (
+    <main className='font-inter bg-primary-green-400 flex h-screen flex-col justify-center gap-2 p-4 sm:items-baseline'>
+      <div className='m-auto flex flex-col items-center gap-10'>
+        <h1 className='font-inter-semibold text-xl text-wrap text-zinc-100 sm:text-2xl'>
+          PRONTUA - Prontuário Acadêmico Uniceplac
+        </h1>
+
+        <div className='flex h-96 w-4xl flex-row items-center rounded-xl bg-zinc-100 shadow-2xl'>
+          <UniceplacLogo className='-ml-28 h-2/4 w-fit rotate-90' />
+
+          <form
+            className='flex h-full w-fit flex-col items-center justify-center gap-4 px-4'
+            onSubmit={handleSubmit(authenticateUser)}
+          >
+            <TextInput
+              label='Acesso:'
+              id='email'
+              type='email'
+              placeholder='Digite seu e-mail'
+              {...register('email')}
+              error={!!errors.email}
+              errorMessage={errors.email?.message ?? null}
+            />
+
+            <TextInput
+              label='Senha:'
+              id='password'
+              type='password'
+              placeholder='Digite sua senha'
+              {...register('password')}
+              error={!!errors.password}
+              errorMessage={errors.password?.message ?? null}
+            />
+
+            <button
+              type='submit'
+              className='bg-primary-green-400 font-inter-bold hover:bg-primary-green-500 h-10 w-full transform rounded-sm text-white shadow-sm transition duration-300 hover:shadow-md'
+            >
+              Entrar
+            </button>
+          </form>
+        </div>
+      </div>
+    </main>
+  );
 }
