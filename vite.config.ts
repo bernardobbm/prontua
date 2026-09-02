c880ae1d56a7c23154d2c51f6c5b1c886473c6f5
bernardobbm <bernardobbm334@gmail.com>
2026-09-01 23:02:50 -0300

    feat: Adição da biblioteca vite-plugin-svgr
    A biblioteca citada está sendo utilizada para manipulação de arquivos vetoriais dentro do sistema.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 import { defineConfig } from 'vite';
+import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,6 +16,7 @@ export default defineConfig({
     }),
     react(),
     tailwindcss(),
+    svgrPlugin(),
   ],
   resolve: {
     alias: {
