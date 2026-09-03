234c1f8c7f87b714b6e38bbbd5da3df5b52f9997
bernardobbm <bernardobbm334@gmail.com>
2026-09-02 23:37:30 -0300

    refactor(monorepo): Migra o repositório para estrutura monorepo
    - organiza aplicações em apps/
    - organiza códigos compartilhados em packages/
    - configura gerenciamento do workspace
    - atualiza configurações de projeto e ferramentas
    - preserva o comportamento atual da aplicação

--- config/eslint-config/vite.js
+++ config/eslint-config/vite.js
@@ -5,8 +5,6 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-
 export default defineConfig([
   globalIgnores(['dist', './**/routeTree.gen.ts']),
   {
@@ -20,22 +18,11 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
-
-      eslintPluginPrettierRecommended,
     ],
     languageOptions: {
       globals: globals.browser,
     },
     rules: {
-      'prettier/prettier': [
-        'error',
-        {
-          plugins: ['prettier-plugin-tailwindcss'],
-          singleQuote: true,
-          trailingComma: 'all',
-          jsxSingleQuote: true,
-        },
-      ],
       'react-refresh/only-export-components': 'off',
     },
   },
